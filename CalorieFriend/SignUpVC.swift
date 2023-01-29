f13a0aa0900eacf2493519bd886d3133902bec96
ChrisBryann <christopher_bryan@rocketmail.com>
2023-01-28 17:06:21 -0800

    Added signup page and cross functionality

---
+++ CalorieFriend/SignUpVC.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpVC.swift
+//  CalorieFriend
+//
+//  Created by Christopher Bryan on 28/01/23.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
