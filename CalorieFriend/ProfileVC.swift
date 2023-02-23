d23ee210afe03d8ed0b20390b18857f318f2fc24
Epsil-m <danielatnight@gmail.com>
2023-02-23 14:08:46 -0800

    Implemented sex and goal selection, updated TDEE calc

--- CalorieFriend/ProfileVC.swift
+++ CalorieFriend/ProfileVC.swift
@@ -12,28 +12,89 @@ class ProfileVC: UIViewController {
     @IBOutlet private var height: UITextField!
     @IBOutlet private var weight: UITextField!
     @IBOutlet private var age: UITextField!
+    @IBOutlet private var sexSelectButton: UIButton!
+    @IBOutlet private var goalSelectButton: UIButton!
     
     let defaults = UserDefaults.standard
 
-    
     struct Keys {
         static let userAge = "userAge"
         static let userHeight = "userHeight"
         static let userName = "userName"
         static let userWeight = "userWeight"
+        static let userSex = "userSex"
+        static let userGoal = "userGoal"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        checkProfile()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         checkProfile()
-        
     }
-
+    
+    // sets up display of popup button with userDefault saved userGoal
+    func setGoalButton(selected: String) {
+        let setAction = {(action : UIAction) in print(action.title)}
+        
+        switch selected {
+        case "Maintain Current Weight (+0 kCals)":
+            goalSelectButton.menu = UIMenu(children : [
+                UIAction(title: "Select", handler: setAction),
+                UIAction(title: "Maintain Current Weight (+0 kCals)", state : .on, handler: setAction),
+                UIAction(title: "Gain Weight (+500 kCals)", handler: setAction),
+                UIAction(title: "Lose Weight (-500 kCals)", handler: setAction)])
+        case "Gain Weight (+500 kCals)":
+            goalSelectButton.menu = UIMenu(children : [
+                UIAction(title: "Select", handler: setAction),
+                UIAction(title: "Maintain Current Weight (+0 kCals)", handler: setAction),
+                UIAction(title: "Gain Weight (+500 kCals)", state : .on, handler: setAction),
+                UIAction(title: "Lose Weight (-500 kCals)", handler: setAction)])
+        case "Lose Weight (-500 kCals)":
+            goalSelectButton.menu = UIMenu(children : [
+                UIAction(title: "Select", handler: setAction),
+                UIAction(title: "Maintain Current Weight (+0 kCals)", handler: setAction),
+                UIAction(title: "Gain Weight (+500 kCals)", handler: setAction),
+                UIAction(title: "Lose Weight (-500 kCals)", state : .on, handler: setAction)])
+        default:
+            goalSelectButton.menu = UIMenu(children : [
+                UIAction(title: "Select", state : .on, handler: setAction),
+                UIAction(title: "Maintain Current Weight (+0 kCals)", handler: setAction),
+                UIAction(title: "Gain Weight (+500 kCals)", handler: setAction),
+                UIAction(title: "Lose Weight (-500 kCals)", handler: setAction)])
+        }
+        
+        goalSelectButton.showsMenuAsPrimaryAction = true
+        goalSelectButton.changesSelectionAsPrimaryAction = true
+    }
+    
+    // sets up display of popup button with userDefault saved userSex
+    func setSexButton(selected: String) {
+        let setAction = {(action : UIAction) in print(action.title)}
+        
+        switch selected {
+        case "Male":
+            sexSelectButton.menu = UIMenu(children : [
+                UIAction(title: "Select", handler:  setAction),
+                UIAction(title: "Male",state : .on, handler:  setAction),
+                UIAction(title: "Female", handler:  setAction)])
+        case "Female":
+            sexSelectButton.menu = UIMenu(children : [
+                UIAction(title: "Select", handler:  setAction),
+                UIAction(title: "Male", handler:  setAction),
+                UIAction(title: "Female",state : .on, handler:  setAction)])
+        default:
+            sexSelectButton.menu = UIMenu(children : [
+                UIAction(title: "Select",state : .on, handler:  setAction),
+                UIAction(title: "Male", handler:  setAction),
+                UIAction(title: "Female", handler:  setAction)])
+        }
+        
+        sexSelectButton.showsMenuAsPrimaryAction = true
+        sexSelectButton.changesSelectionAsPrimaryAction = true
+    }
     
     @IBAction func saveButton(_ sender: UIButton) {
         saveProfile()
@@ -45,7 +106,9 @@ class ProfileVC: UIViewController {
         defaults.set(height.text!, forKey: Keys.userHeight)
         defaults.set(weight.text!, forKey: Keys.userWeight)
         defaults.set(age.text!, forKey: Keys.userAge)
-        defaults.set(String(calculateTDEE()), forKey: "userGoal")
+        defaults.set(sexSelectButton.currentTitle, forKey: Keys.userSex)
+        defaults.set(goalSelectButton.currentTitle, forKey: Keys.userGoal)
+        defaults.set(String(calculateTDEE()), forKey: "userTDEE")
     }
     
     // Check if there are any saved values, if so update
@@ -54,6 +117,12 @@ class ProfileVC: UIViewController {
         let savedAge = defaults.value(forKey: Keys.userAge) as? String ?? ""
         let savedHeight = defaults.value(forKey: Keys.userHeight) as? String ?? ""
         let savedWeight = defaults.value(forKey: Keys.userWeight) as? String ?? ""
+        let savedSex = defaults.value(forKey: Keys.userSex) as? String ?? "Select"
+        let savedGoal = defaults.value(forKey: Keys.userGoal) as? String ?? "Select"
+        
+        setSexButton(selected: savedSex)
+        setGoalButton(selected: savedGoal)
+        
         name.text = savedName
         age.text = savedAge
         height.text = savedHeight
@@ -67,6 +136,8 @@ class ProfileVC: UIViewController {
         let savedAge = defaults.value(forKey: Keys.userAge) as? String ?? ""
         let savedHeight = defaults.value(forKey: Keys.userHeight) as? String ?? ""
         let savedWeight = defaults.value(forKey: Keys.userWeight) as? String ?? ""
+        let savedSex = defaults.value(forKey: Keys.userSex) as? String ?? "Select"
+        let savedGoal = defaults.value(forKey: Keys.userGoal) as? String ?? "Select"
         
         if (savedAge != "" && savedHeight != "" && savedWeight != "") {
             let intWeight = Int(savedWeight) ?? 0
@@ -75,15 +146,22 @@ class ProfileVC: UIViewController {
             let weightCalc = 10 * (Double(intWeight) / 2.205)
             let heightCalc = 6.25 * Double(intHeight) * 2.54
             let AgeCalc = 5 * Double(intAge) + 5
+            
             tdee =  Int(weightCalc + heightCalc - AgeCalc)
-            print(tdee)
+            
+            if (savedSex == "Male") {
+                tdee += 5
+            } else if (savedSex == "Female") {
+                tdee -= 161
+            }
+            
+            if (savedGoal == "Gain Weight (+500 kCals)") {
+                tdee += 500
+            } else if (savedGoal == "Lose Weight (-500 kCals)") {
+                tdee -= 500
+            }
         }
-    //two formulas, 1 for female and 1 for male
-    // (10 * weight (kg)) + (6.25 * height (cm)) - (5 * age)
-    //                                  + 5 for males
-    //                                  - 161 for females
         
         return Int(tdee)
     }
-
 }
